6c62f8ae8eb1b84b70cbf3e9bba7c61aea1c1483
Kevin Joy <kevinfjoy@gmail.com>
2020-12-20 16:50:02 -0500

    adding python package info

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    depends = fh.read()
+
+setuptools.setup(
+    name="rei-kjoy11", # Replace with your own username
+    version="0.0.1",
+    author="Kevin Joy",
+    author_email="kevinfjoy@gmail.com",
+    description="Package queries sdat database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/frankij11/rei",
+    packages=setuptools.find_packages(),
+    install_requires = depends,
+    #include_package_data = True,
+    package_data={"": ['data/*']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
