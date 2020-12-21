acf9ca11b1c7b1004b84f864acf82f88933fef31
frankij11 <kevinfjoy@gmail.com>
2020-12-20 19:27:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/frankij11/rei",
-    packages=setuptools.find_packages(where=".."),
+    packages=setuptools.find_packages(),
     install_requires = depends,
     #include_package_data = True,
     package_data={"": ['data/*']},
